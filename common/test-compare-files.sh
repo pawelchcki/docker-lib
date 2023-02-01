e699c3c4bb806fb6bbf8d67b07abaf8da50fb239
Pawel Chojnacki <pawelchcki@gmail.com>
2023-02-01 14:12:02 +0100

    fix compatibility with sha256 util

--- common/test-compare-files.sh
+++ common/test-compare-files.sh
@@ -2,4 +2,4 @@
 # TODO: move this to own test utils package
 file_a=$1; shift
 file_b=$1; shift
-echo "$(sha256sum $file_a | cut -d ' ' -f 1) $file_b" | sha256sum -c
+sha256sum $file_a | sed -e "s/$file_a/$file_b/" | sha256sum -c
